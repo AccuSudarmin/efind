6de31e3f2b650cd018c28c52825db07d595ece83
aiiaccu <accusudarmin@gmail.com>
2015-08-25 14:05:47 +0800

    untuk apa

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>Event Finder Mockup</title>
+      <link rel="stylesheet" href="/public/stylesheet/screen.css" media="screen" title="no title" charset="utf-8">
+      <link rel="stylesheet" href="/public/stylesheet/print.css" media="print" title="no title" charset="utf-8">
+   </head>
+   <body>
+
+   </body>
+</html>
