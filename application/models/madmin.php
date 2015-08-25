f7268ca3b9fe813d5383407ffaecc1d4a32e58b7
Azrul <accusudarmin@gmail.com>
2015-08-25 15:35:06 +0800

    Update Accu

---
+++ application/models/madmin.php
@@ -0,0 +1,19 @@
+<?php
+   defined('BASEPATH') OR exit('No direct script access allowed');
+
+   class Madmin extends CI_Model {
+
+      public function getByName($data) {
+
+         // $this->dbsql->select(array(
+         //    'table' => 'admin' ,
+         //    'where' => "amName ='" . $data . "'"
+         // ));
+         //
+         // $result = $this->dbsql->result();
+
+         // return $result;
+      }
+
+   }
+?>
