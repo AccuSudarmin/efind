08445bfc99cf1675b199170926ce67cb50445e30
aiiaccu <accusudarmin@gmail.com>
2015-08-25 14:43:29 +0800

    Update View

---
+++ application/views/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>Event Finder Mockup</title>
+      <link rel="stylesheet" href="/public/stylesheet/screen.css" media="screen, projection" title="no title" charset="utf-8">
+      <link rel="stylesheet" href="/public/stylesheet/print.css" media="print" title="no title" charset="utf-8">
+      <link rel="stylesheet" href="/public/stylesheet/ie.css" media="screen" title="no title" charset="utf-8">
+   </head>
+   <body>
+      <header>
+         <div class="logo"> </div>
+         <nav>
+            <ul class='left-ul'>
+               <li>Home</li>
+               <li>Sub Menu 1 </li>
+               <li>Sub Menu 2 </li>
+               <li>Sub Menu 3 </li>
+            </ul>
+
+            <ul class='right-ul'>
+               <li> Search Icon
+                  <ul> <li> <input type='text'> </li> </ul>
+               </li>
+            </ul>
+         </nav>
+      </header>
+   </body>
+</html>
