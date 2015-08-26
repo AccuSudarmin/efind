eab9b08bf048d153fd603e8b5d04b49b5ce4494d
Azrul <accusudarmin@gmail.com>
2015-08-26 10:56:49 +0800

    Update Admin Interface

--- application/views/administrator/login.php
+++ application/views/administrator/login.php
@@ -1,50 +1,52 @@
 <!DOCTYPE HTML>
-
 <html>
 
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Login to Admin Eventfinder</title>
-</head>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="<?php echo base_url('public/css/admin-style.css'); ?>">
+		<title>Login to Admin Eventfinder</title>
+	</head>
 
-<body>
+	<body>
+		<header>
+			<nav class="navbar">
+				<ul>
+					<li>
+						<a href="#" class="button left-arrow image-left">Return to website</a> 
+					</li>
+				</ul>
+			</nav>
+			<div>
+				LOGIN TO ADMIN <br>
+				Enter you credentials below
+			</div>
+		</header>
 
-	<header>
-		<nav>
-			<ul>
-				<li>Return to websiter</li>
-			</ul>
-		</nav>
-		<div>
-			LOGIN TO ADMIN <br>
-			Enter you credentials below
-		</div>
-	</header>
+		<section>
 
-	<section>
+			<div>
+				<p>
+					USERNAME <br>
+					<input type="text" name="username">
+				</p>
+				<p>
+					PASSWORD <br>
+					<input type="password" name="password">
+				</p>
+			</div>
 
-		<div>
-			<p>
-				USERNAME <br>
-				<input type="text" name="username">
-			</p>
-			<p>
-				PASSWORD <br>
-				<input type="password" name="password">
-			</p>
-		</div>
+			<div>
+				Just click on the "LOGIN" button to continue, no login information required.
+			</div>
 
-		<div>
-			Just click on the "LOGIN" button to continue, no login information required.
-		</div>
-	</section>
+		</section>
 
-	<footer>
-		<p>Copyright 2015 Eventfinder All rights reserved</p>
-	</footer>
+		<footer>
+			<p>Copyright 2015 Eventfinder All rights reserved</p>
+		</footer>
 
-</body>
+	</body>
 
 </html>
