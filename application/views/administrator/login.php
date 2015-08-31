e506ec0421ce4c21e237f68174e3370cd895e8c6
Azrul <accusudarmin@gmail.com>
2015-09-01 01:50:37 +0800

    Update Interface

--- application/views/administrator/login.php
+++ application/views/administrator/login.php
@@ -14,13 +14,9 @@
 
 	<body>
 		<header class="page-header">
-			<nav class="navbar">
-				<ul>
-					<li>
-						<a href="#" class="button left-arrow-icon image-left">Return to website</a>
-					</li>
-				</ul>
-			</nav>
+			<a href="#">
+				<az-button class="button left-arrow-icon image-left" name="return" width="145px" height="38px" color="#3f4551"> Return to website </az-button>
+			</a>
 		</header>
 
 		<section class="page-title">
--- application/views/administrator/navbar.php
+++ application/views/administrator/navbar.php
@@ -1,9 +1,10 @@
 <header class="page-header">
-   <nav class="navbar">
-      <ul>
-         <li>
-            <a href="#" class="button left-arrow-icon image-left">Return to website</a>
-         </li>
-      </ul>
-   </nav>
+   <a href="#">
+      <az-button class="button left-arrow-icon image-left" name="return" width="145px" height="38px" color="#3f4551"> Return to website </az-button><!--
+   --></a>
+   <div class="bracket"></div>
+   <a href="#">
+      <az-button class="button left-arrow-icon image-left" name="return" width="145px" height="38px" color="#3f4551">Return to website</az-button>
+   </a>
+
 </header>
