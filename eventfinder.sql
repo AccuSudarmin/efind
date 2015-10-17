06c4903ca3764d2cb223dab3a733eb093b326f99
Azrul Sudarmin <accusudarmin@gmail.com>
2015-10-16 23:23:35 -0700

    Update

--- eventfinder.sql
+++ eventfinder.sql
@@ -1,13 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Sep 03, 2015 at 03:32 PM
--- Server version: 5.5.24-log
--- PHP Version: 5.3.13
+-- Host: 127.0.0.1
+-- Generation Time: Oct 17, 2015 at 08:23 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
@@ -49,14 +49,45 @@ INSERT INTO `admin` (`amId`, `amName`, `amPassword`, `amSalt`) VALUES
 
 CREATE TABLE IF NOT EXISTS `article` (
   `arId` int(11) NOT NULL AUTO_INCREMENT,
-  `arTitle` varchar(100) NOT NULL,
-  `arContent` text NOT NULL,
-  `arDate` date NOT NULL,
-  `arPict` varchar(100) NOT NULL,
-  `arURL` varchar(100) NOT NULL,
-  `arAuthor` int(11) NOT NULL,
+  `arTitle` varchar(100) DEFAULT NULL,
+  `arContent` text,
+  `arDateStart` date DEFAULT NULL,
+  `arDateEnd` date DEFAULT NULL,
+  `arPict` varchar(100) DEFAULT NULL,
+  `arURL` varchar(100) DEFAULT NULL,
+  `arAuthor` int(11) DEFAULT NULL,
+  `arTicketPrice` text,
+  `arBarcode` varchar(100) DEFAULT NULL,
+  `arCategory` int(11) DEFAULT NULL,
+  `arContact` text,
+  `arDatePost` varchar(11) DEFAULT NULL,
+  `arStatus` enum('1','0') NOT NULL,
   PRIMARY KEY (`arId`),
-  KEY `arAuthor` (`arAuthor`)
+  KEY `arAuthor` (`arAuthor`),
+  KEY `arCategory` (`arCategory`),
+  KEY `arPict` (`arPict`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
+
+--
+-- Dumping data for table `article`
+--
+
+INSERT INTO `article` (`arId`, `arTitle`, `arContent`, `arDateStart`, `arDateEnd`, `arPict`, `arURL`, `arAuthor`, `arTicketPrice`, `arBarcode`, `arCategory`, `arContact`, `arDatePost`, `arStatus`) VALUES
+(42, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit', '                                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2015-10-15', '2015-10-15', 'http://localhost/eventfinder/public/userfiles/image\\3.jpg', 'Lorem-ipsum-dolor-sit-amet,-consectetur-adipisicing-elit', 1, 'Rp. 2000.000,-', 'http://eventfinder.co.id', 2, 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore', '2015-10-14', ''),
+(43, 'Sport Event Presented By Eventfinder', 'Sport Event Presented By Eventfinder&nbsp;Sport Event Presented By Eventfinder&nbsp;Sport Event Presented By Eventfinder', '2015-10-15', '2015-10-17', 'http://localhost/eventfinder/public/userfiles/image\\photo3.png', 'Sport-Event-Presented-By-Eventfinder', 1, 'Rp. 200.000,-', 'http://eventfinder.co.id', 3, 'Phone: 0411 70707070\r\nEmail: contact@eventfinder.co.id', '2015-10-15', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hit_article`
+--
+
+CREATE TABLE IF NOT EXISTS `hit_article` (
+  `haId` int(11) NOT NULL AUTO_INCREMENT,
+  `haTotal` int(50) DEFAULT NULL,
+  `haDate` varchar(50) DEFAULT NULL,
+  `haArticleId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`haId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -74,6 +105,144 @@ CREATE TABLE IF NOT EXISTS `log` (
   KEY `logAdmin` (`logAdmin`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `media`
+--
+
+CREATE TABLE IF NOT EXISTS `media` (
+  `mdId` int(11) NOT NULL,
+  `mdLocation` varchar(100) NOT NULL,
+  PRIMARY KEY (`mdId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `media`
+--
+
+INSERT INTO `media` (`mdId`, `mdLocation`) VALUES
+(1, 'http://www.eventfinder.co.id/images/slide2_03.gif'),
+(2, 'http://www.eventfinder.co.id/images/slide_01.gif'),
+(3, 'http://www.eventfinder.co.id/images/slide_05.gif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ref_category`
+--
+
+CREATE TABLE IF NOT EXISTS `ref_category` (
+  `catId` int(11) NOT NULL,
+  `catName` varchar(50) NOT NULL,
+  PRIMARY KEY (`catId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ref_category`
+--
+
+INSERT INTO `ref_category` (`catId`, `catName`) VALUES
+(1, 'Music'),
+(2, 'Exhibition'),
+(3, 'Sport');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ref_map`
+--
+
+CREATE TABLE IF NOT EXISTS `ref_map` (
+  `mapId` int(11) NOT NULL AUTO_INCREMENT,
+  `mapLongitude` varchar(50) DEFAULT NULL,
+  `mapLatitude` varchar(50) DEFAULT NULL,
+  `mapZoom` int(50) DEFAULT NULL,
+  `mapArticleId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`mapId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
+
+--
+-- Dumping data for table `ref_map`
+--
+
+INSERT INTO `ref_map` (`mapId`, `mapLongitude`, `mapLatitude`, `mapZoom`, `mapArticleId`) VALUES
+(39, '119.42392891658528', '-5.13705695288282', 15, 42),
+(40, '119.41178306937218', '-5.135069401600222', 9, 43),
+(41, '119.43407247774303', '-5.319064392442244', 13, 43);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ref_social_media`
+--
+
+CREATE TABLE IF NOT EXISTS `ref_social_media` (
+  `smId` int(11) NOT NULL AUTO_INCREMENT,
+  `smTwitter` varchar(50) DEFAULT NULL,
+  `smFacebook` varchar(50) DEFAULT NULL,
+  `smLine` varchar(50) DEFAULT NULL,
+  `smPath` varchar(50) DEFAULT NULL,
+  `smInstagram` varchar(50) DEFAULT NULL,
+  `smArticleId` int(11) DEFAULT NULL,
+  PRIMARY KEY (`smId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=41 ;
+
+--
+-- Dumping data for table `ref_social_media`
+--
+
+INSERT INTO `ref_social_media` (`smId`, `smTwitter`, `smFacebook`, `smLine`, `smPath`, `smInstagram`, `smArticleId`) VALUES
+(39, '@twitter1', '@facebook1', '@line1', '@path1', '@instagram1', 42),
+(40, '@eventfinderid', '@eventfinderid', '@eventfinderid', '@eventfinderid', '@eventfinderid', 43);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `slider`
+--
+
+CREATE TABLE IF NOT EXISTS `slider` (
+  `slId` int(11) NOT NULL AUTO_INCREMENT,
+  `slTitle` text,
+  `slPict` text,
+  `slOrder` int(11) NOT NULL,
+  PRIMARY KEY (`slId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `slider`
+--
+
+INSERT INTO `slider` (`slId`, `slTitle`, `slPict`, `slOrder`) VALUES
+(4, 'Slider 3', 'http://localhost/eventfinder/public/userfiles/image\\photo3.png', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitor_today`
+--
+
+CREATE TABLE IF NOT EXISTS `visitor_today` (
+  `vtdId` int(11) NOT NULL AUTO_INCREMENT,
+  `vtdDate` varchar(50) NOT NULL,
+  `vtdTotal` int(11) NOT NULL,
+  PRIMARY KEY (`vtdId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `visitor_total`
+--
+
+CREATE TABLE IF NOT EXISTS `visitor_total` (
+  `vtId` int(11) NOT NULL AUTO_INCREMENT,
+  `vtDate` varchar(50) DEFAULT NULL,
+  `vtTotal` int(11) DEFAULT NULL,
+  PRIMARY KEY (`vtId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
 --
 -- Constraints for dumped tables
 --
@@ -82,7 +251,8 @@ CREATE TABLE IF NOT EXISTS `log` (
 -- Constraints for table `article`
 --
 ALTER TABLE `article`
-  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`arAuthor`) REFERENCES `admin` (`amId`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`arAuthor`) REFERENCES `admin` (`amId`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `article_ibfk_2` FOREIGN KEY (`arCategory`) REFERENCES `ref_category` (`catId`) ON UPDATE CASCADE;
 
 --
 -- Constraints for table `log`
