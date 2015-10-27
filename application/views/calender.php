749031fa284755fc224c0cd38870977c9085ce41
Azrul Sudarmin <accusudarmin@gmail.com>
2015-10-28 07:21:50 +0800

    Adding algorithm to show article

--- application/views/calender.php
+++ application/views/calender.php
@@ -1,93 +1,69 @@
+<div class="calendar-container">
+   <form action= "">
+      <select name ="month">
+         <?php
+            for ($i = 0; $i <= 12; $i++){
+               echo "<option value = " . date('m') . "> " . date('F') . "</option>";
+            }
+         ?>
+      </select>
 
-      <div class="calendar-container">
-         <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-         <script src="<?php echo base_url('public/js/calendar.js');?>"></script> -->
+      <input type = "number" min="1999" value="1999" name="Year"> </input>
+      <button><i class="fa fa-search"> </i></button>
+   </form>
 
-         <!-- <h2> January 2015 </h2> <button><i class="fa fa-search"> </i></button> -->
-         <form action= "">
-           <select name ="month">
-           <option value = "January"> January </option>
-           <option value = "February"> February </option>
-           <option value = "March"> March </option>
-           <option value = "April"> April </option>
-           <option value = "May"> May </option>
-           <option value = "June"> June </option>
-           <option value = "July"> July </option>
-           <option value = "August"> August </option>
-           <option value = "September"> September </option>
-           <option value = "October"> October </option>
-           <option value = "November"> November </option>
-           <option value = "Desember"> Desember </option>
-         </select>
+   <ul class="calendar-ul">
+      <?php
+         $i = 1; foreach ($event as $key => $value):
 
-         <input type = "number" min="1999" value="1999" name="Year"> </input>
+         $date = date_create_from_format('Y-m-d' , $year . '-' . $month . '-' .$key);
 
-         <button><i class="fa fa-search"> </i></button>
+         if ($i == 1):
+      ?>
+      <li class="main-calendar">
+        <ul>
+      <?php endif; ?>
 
-        </form>
-         <ul class="calendar-ul">
-            <!-- <li class="thearrow"><a href="#" class="arrow-prev"><i class="fa fa-angle-left"></i></a></li> -->
-            <li class="main-calendar">
-                 <ul>
-                 <li class="selected">
-                     <div class="isi-calender">
-                        <div class="calender-date"> 01 | Mon </div>
-                        <article>
-                           <ul>
-                              <ol>
-                                 <a is="az-anchorajax"
-                                 action='<?php echo site_url('music/view/lorem')  ?>'
-                                 method='click'
-                                 > Lorem </a>
-                              </ol>
-                              <ol> Ipsum </ol>
-                           </ul>
-                        </article>
-                     </div>
-                     <img src="<?php echo base_url('public/img/music1.jpg');?>">
-                     <img src="<?php echo base_url('public/img/music5.jpg');?>">
-                 </li>
-                 <li><div class="isi-calender">
-                      <div class="calender-date"> 01 | Monday </div> <article> <ul> <ol><a href="#"> Lorem </a> </ol> <ol> Ipsum </ol> </ul> </article> </div><img src="<?php echo base_url('public/img/music5.jpg');?>"></li>
-                 <li><div class="isi-calender">
-                      <div class="calender-date"> 01 | Monday </div> <article> <ul> <ol><a href="#"> Lorem </a> </ol> <ol> Ipsum </ol> </ul> </article> </div><img src="<?php echo base_url('public/img/3.jpg');?>"></li>
-                 <li><div class="isi-calender">
-                      <div class="calender-date"> 01 | Monday </div> <article> <ul> <ol><a href="#"> Lorem </a> </ol> <ol> Ipsum </ol> </ul> </article> </div><img src="<?php echo base_url('public/img/music4.jpg');?>"></li>
-                </ul>
-             </li>
-           <li class="main-calendar">
-                <ul>
-                <li class="selected"><div class="isi-calender">
-                     <div class="calender-date"> 01 | Mon </div> <article> <ul> <ol><a href="#"> Lorem </a> </ol> <ol> Ipsum </ol> </ul> </article> </div>
-                   <img src="<?php echo base_url('public/img/music1.jpg');?>">
-                   <img src="<?php echo base_url('public/img/music5.jpg');?>">
-                </li>
-                <li><div class="isi-calender">
-                     <div class="calender-date"> 01 | Monday </div> <article> <ul> <ol><a href="#"> Lorem </a> </ol> <ol> Ipsum </ol> </ul> </article> </div><img src="<?php echo base_url('public/img/music5.jpg');?>"></li>
-                <li><div class="isi-calender">
-                     <div class="calender-date"> 01 | Monday </div> <article> <ul> <ol><a href="#"> Lorem </a> </ol> <ol> Ipsum </ol> </ul> </article> </div><img src="<?php echo base_url('public/img/3.jpg');?>"></li>
-                <li><div class="isi-calender">
-                     <div class="calender-date"> 01 | Monday </div> <article> <ul> <ol><a href="#"> Lorem </a> </ol> <ol> Ipsum </ol> </ul> </article> </div><img src="<?php echo base_url('public/img/music4.jpg');?>"></li>
-               </ul>
+            <li>
+               <div class="isi-calender">
+                  <div class="calender-date"> <?php echo $key; ?> | <?php echo date_format($date, 'l'); ?> </div>
+                  <article>
+                     <ul>
+
+                        <?php foreach ($value as $data): ?>
+                        <ol>
+                           <a is="az-anchorajax"
+                              action='<?php echo site_url('music/show/' . $data->arId);  ?>'
+                              method='click'
+                           > <?php echo $data->arTitle; ?> </a>
+                        </ol>
+                        <?php endforeach; ?>
+
+                     </ul>
+                  </article>
+               </div>
+
+               <?php
+                  if (count($value) > 0):
+                     foreach ($value as $data):
+               ?>
+                  <?php if (!empty($data->arPict)): ?>
+                     <img src="<?php echo $data->arPict; ?>">
+                  <?php endif; ?>
+               <?php
+                     endforeach;
+                  else:
+               ?>
+               <img src="<?php echo base_url('public/img/music5.jpg');?>">
+               <?php endif; ?>
             </li>
-            <li class="main-calendar">
-                 <ul>
-                 <li class="selected"><div class="isi-calender">
-                      <div class="calender-date"> 01 | Mon </div> <article> <ul> <ol><a href="#"> Lorem </a> </ol> <ol> Ipsum </ol> </ul> </article> </div>
-                    <img src="<?php echo base_url('public/img/music1.jpg');?>">
-                    <img src="<?php echo base_url('public/img/music5.jpg');?>">
-                 </li>
-                 <li><div class="isi-calender">
-                      <div class="calender-date"> 01 | Monday </div> <article> <ul> <ol><a href="#"> Lorem </a> </ol> <ol> Ipsum </ol> </ul> </article> </div><img src="<?php echo base_url('public/img/music5.jpg');?>"></li>
-                 <li><div class="isi-calender">
-                      <div class="calender-date"> 01 | Monday </div> <article> <ul> <ol><a href="#"> Lorem </a> </ol> <ol> Ipsum </ol> </ul> </article> </div><img src="<?php echo base_url('public/img/3.jpg');?>"></li>
-                 <li><div class="isi-calender">
-                      <div class="calender-date"> 01 | Monday </div> <article> <ul> <ol><a href="#"> Lorem </a> </ol> <ol> Ipsum </ol> </ul> </article> </div><img src="<?php echo base_url('public/img/music4.jpg');?>"></li>
-                </ul>
-             </li>
 
-           <!-- <li class="thearrow rights" style="right:0;"> <a href="#" class="arrow-next"><i class="fa fa-angle-right"></i></a></li> -->
+      <?php if ($i >= 4): $i=0; ?>
          </ul>
+      </li>
+   <?php endif; ?>
 
-         <!-- <button class="left"> <i class="fa fa-chevron-circle-down"></i></button> -->
-      </div>
+      <?php $i++; endforeach; ?>
+   </ul>
+
+</div>
