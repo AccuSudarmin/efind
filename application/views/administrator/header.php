68a6479f817cfcbb286e64b0eb202cf1c5bb4c74
Azrul Sudarmin <accusudarmin@gmail.com>
2015-10-30 13:14:01 +0800

    update link for home

--- application/views/administrator/header.php
+++ application/views/administrator/header.php
@@ -1,5 +1,5 @@
 <header class="page-header">
-   <a href="#">
+   <a href="<?php echo site_url(); ?>">
       <az-button class="button left-arrow-icon image-left" name="return" width="145px" height="38px" color="#3f4551"> Return to website </az-button><!--
    --></a>
    <a href="<?php echo site_url('/admin/login/dologout'); ?>" class="float-right">
